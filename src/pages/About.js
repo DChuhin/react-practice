ef2028b3a5776aa196978df2215b7e2cfee37f64
daniilchukhin <daniil.chukhin@helmes.ee>
2020-01-01 12:21:19 +0300

    Initial commit from Create React App

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const About = () => (<div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Fluid jumbotron</h1>
+      <p className="lead">This is a jumbotron.</p>
+    </div>
+  </div>);
